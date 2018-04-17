879975f81cf35029338f6e4174e43c51e16e0f2d
TowerZmj <392519739@qq.com>
2018-04-17 11:10:23 +0800

    add tensor build file

---
+++ dataclean.py
@@ -0,0 +1,11 @@
+from tensor import Tensor
+
+def transform_csv_to_liner_tensor(csv_file_name):
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ tensor.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+
+class Tensor:
+    def __init__(self, tensor_info):
+        self.merchant_id = tensor_info['merchant_id']
+        self.activity_log = tensor_info['activity_log']
+        
+    def build_tensor():
+        x_length = len(self.merchant_id)
+        y_length = 4
+        self.tensor = np.zeros((x_length, y_length))
+        
+        merchant_id_arr = self.merchant_id.values
+        activity_log_arr = self.activity_log.values
+        for i in range(x_length):
+            specific_merchant_activity_log = activity_log_arr[i]
+            specific_merchant_type_log = self.analysis_activity_log(\
+                specific_merchant_activity_log)
+            specific_merchant_type_log_series = pd.Series(\
+                specific_merchant_type_log)
+            type_value_counts = specific_merchant_type_log_series.\
+                value_counts()
+            type_index = type_value_counts.index
+            for j in range(y_length):
+                if j in type_index:
+                    self.tensor[i][j] = type_value_counts[j]
+      
+        
+    
+    def analysis_activity_log(activity_log):
+        activity_type_log = list()
+        activity_log_list = activity_log.split('#')
+        for log in activity_log_list:
+            log_field = log.split(':')
+            activity_type_log.append(int(log_field[4]))
+        
+        return activity_type_log
+    
+    def is_crawler():
+        pass
+
+    def is_one_click():
+        pass
